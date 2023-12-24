aa260494a29ed7730bcb165c986ac8858ebb0d41
Will Crichton <wcrichto@cs.stanford.edu>
2023-12-24 15:55:31 -0800

    Add epubs w/ LFS

---
+++ epubs/bene-tests/make-epub.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+rm -f $1.epub
+set -e
+pushd $1
+echo -n "application/epub+zip" > mimetype
+zip -0 -X ../$1.epub mimetype
+rm -f mimetype
+zip -r ../$1.epub EPUB META-INF
+popd
+epubcheck $1.epub
