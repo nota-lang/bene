b593d921acbc78a1ff09e8748281b8deed119f72
Auguste Baum <auguste.apple@gmail.com>
2025-04-15 17:56:48 +0200

    try to compile tauri app

--- shell.nix
+++ shell.nix
@@ -7,6 +7,21 @@ let
   depot = (builtins.getFlake "https://github.com/cognitive-engineering-lab/depot/tarball/main").packages.x86_64-linux.default;
 in
   pkgs.mkShell {
+    nativeBuildInputs = [
+      pkgs.openssl
+      pkgs.gobject-introspection
+      pkgs.gtk3
+      pkgs.gtk3.dev
+      pkgs.atk
+      pkgs.glib
+      pkgs.cairo
+      pkgs.gdk-pixbuf
+      pkgs.libsoup_3
+      pkgs.webkitgtk_4_1
+      pkgs.pango
+      pkgs.pango.dev
+    ];
+
     packages = with pkgs; [
       cargo
       cargo-tauri
@@ -22,5 +37,18 @@ in
       pnpm
       # biome
       wasm-pack
+    ];
+
+    PKG_CONFIG_PATH = pkgs.lib.concatStringsSep ":" [
+      "${pkgs.openssl.dev}/lib/pkgconfig"
+      "${pkgs.gobject-introspection.dev}/lib/pkgconfig"
+      "${pkgs.gtk3.dev}/lib/pkgconfig"
+      "${pkgs.atk.dev}/lib/pkgconfig"
+      "${pkgs.glib.dev}/lib/pkgconfig"
+      "${pkgs.cairo.dev}/lib/pkgconfig"
+      "${pkgs.gdk-pixbuf.dev}/lib/pkgconfig"
+      "${pkgs.libsoup_3.dev}/lib/pkgconfig"
+      "${pkgs.webkitgtk_4_1.dev}/lib/pkgconfig"
+      "${pkgs.pango.dev}/lib/pkgconfig"
     ];
   }
